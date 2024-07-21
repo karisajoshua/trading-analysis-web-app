8e3dbe4d31fb63bf21a1056c2b685e94b197b5a7
Joshua Karisa <55674650+karisajoshua@users.noreply.github.com>
2024-07-22 00:36:32 +0300

    HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExampleComponent from '../components/ExampleComponent';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <ExampleComponent />
+    </div>
+  );
+}
+
+export default HomePage;
